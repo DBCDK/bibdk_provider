e38ae29cb5861e50c84713295420de182ad5d3b6
pjohans <pjohans@gmail.com>
2012-09-26 07:00:58 +0200

    added column orderagency

--- sql/bibdk_favourite.sql
+++ sql/bibdk_favourite.sql
@@ -1,7 +1,8 @@
 drop table bibdk_favourite;
-create table bibdk_favourite( favourite_id serial, username character varying(64), agencyid character varying(16));
+create table bibdk_favourite( favourite_id serial, username character varying(64) not null, agencyid character varying(16) not null, orderagency boolean not null default FALSE);
 alter table bibdk_favourite add constraint PK_favourite_id primary key(favourite_id);
 alter table bibdk_favourite add constraint FK_username foreign key(username) references ddbuser(username) on delete cascade;
 create index bibdk_favourite_agencyid_idx on bibdk_favourite (agencyid);
 comment on column bibdk_favourite.username is 'bibdk username is email address. References ddbuser.username. cascade on delete';
 comment on column bibdk_favourite.agencyid is 'id of favourite agency';
+comment on column bibdk_favourite.orderagency is 'selected agency to order from';
