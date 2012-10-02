610ac174472c8bafa85e245b1fff82d76e9a3435
pjohans <pjohans@gmail.com>
2012-10-02 08:34:14 +0200

    added userdata column

--- sql/bibdk_favourite.sql
+++ sql/bibdk_favourite.sql
@@ -1,8 +1,13 @@
 drop table bibdk_favourite;
-create table bibdk_favourite( favourite_id serial, username character varying(64) not null, agencyid character varying(16) not null, orderagency boolean not null default FALSE);
+create table bibdk_favourite( favourite_id serial, username character varying(64) not null, agencyid character varying(16) not null, userdata text, orderagency boolean not null default FALSE);
 alter table bibdk_favourite add constraint PK_favourite_id primary key(favourite_id);
 alter table bibdk_favourite add constraint FK_username foreign key(username) references ddbuser(username) on delete cascade;
 create index bibdk_favourite_agencyid_idx on bibdk_favourite (agencyid);
 comment on column bibdk_favourite.username is 'bibdk username is email address. References ddbuser.username. cascade on delete';
 comment on column bibdk_favourite.agencyid is 'id of favourite agency';
+comment on column bibdk_favourite.userdata is 'serialized userdata. username,pincode etc.';
 comment on column bibdk_favourite.orderagency is 'selected agency to order from';
+
+<!-- TESTDATA -->
+insert into bibdk_favourite(username, agencyid, userdata, orderagency) values('pjohans@gmail.com', '710100', 'HEST', TRUE);
+insert into bibdk_favourite(username, agencyid, userdata, orderagency) values('pjohans@gmail.com', '612934', 'ZEBRA', FALSE);
