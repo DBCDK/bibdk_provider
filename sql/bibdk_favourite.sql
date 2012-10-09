9cfc520c81434f7726b49b304054ce12082a1d48
pjohans <pjohans@gmail.com>
2012-10-09 08:26:33 +0200

    Unique constraint on username,agencyid

--- sql/bibdk_favourite.sql
+++ sql/bibdk_favourite.sql
@@ -2,6 +2,7 @@ drop table bibdk_favourite;
 create table bibdk_favourite( favourite_id serial, username character varying(64) not null, agencyid character varying(16) not null, userdata text, orderagency boolean not null default FALSE);
 alter table bibdk_favourite add constraint PK_favourite_id primary key(favourite_id);
 alter table bibdk_favourite add constraint FK_username foreign key(username) references ddbuser(username) on delete cascade;
+alter table bibdk_favourite add constraint UN_username_agencyid UNIQUE(username,agencyid);
 create index bibdk_favourite_agencyid_idx on bibdk_favourite (agencyid);
 comment on column bibdk_favourite.username is 'bibdk username is email address. References ddbuser.username. cascade on delete';
 comment on column bibdk_favourite.agencyid is 'id of favourite agency';
